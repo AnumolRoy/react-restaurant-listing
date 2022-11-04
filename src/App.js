47d07eae96971fa7b6f99b6ffa9260cc3e12e2d1
Anumol Roy <anumolroy2001@gmail.com>
2022-11-04 17:25:23 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home'
+import Footer from './components/Footer'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import RestDetails from './components/RestDetails';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+<Header/>
+<Routes>
+<Route path='/' element={<Home/>
+}/>
+<Route path='/restaurant/:id' element={<RestDetails/>}/>
+</Routes>
+<Footer/>
+</Router>
+    </div>
+  );
+}
+
+export default App;
